3f79d9bf6208d3ac9f31c998523b04d9d2cff4be
MonicaPoveda <monicapoveda337@gmail.com>
2026-08-19 14:08:59 -0500

    SCRUM-5: Agregar envío de correo de verificación

--- evaluacion/src/main/java/com/evaluacion/evaluacion/model/PRUEBAJIRA.java
+++ evaluacion/src/main/java/com/evaluacion/evaluacion/model/PRUEBAJIRA.java
@@ -2,5 +2,6 @@ package com.evaluacion.evaluacion.model;
 
 public class PRUEBAJIRA {
 
-    SEGUNDA LINEA DE PRUEBA commit 2
+    SEGUNDA LINEA DE PRUEBA commit 
+    tercera linea de prueba commit3
 }
